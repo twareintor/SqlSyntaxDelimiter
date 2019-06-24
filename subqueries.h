294071a62a8d2113414e3cca6299ef05b7965ad2
Claudiu Ciutacu <ciutacu@hotmail.com>
2019-06-24 12:22:23 +0200

    subqueries

--- subqueries.h
+++ subqueries.h
@@ -4,4 +4,4 @@
 
 int getCurrentSubquery(char** szText, const char* szCode, const size_t codeSize, const char* szPos);
 
-int getCurrentSubquery2(char** szText, const char* szCode, const size_t codeSize, const char* szPos);
+
