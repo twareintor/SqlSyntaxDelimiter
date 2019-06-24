3e50770eb3d4c359e1f5a98b2a6be26bc24e374b
Claudiu Ciutacu <ciutacu@hotmail.com>
2019-06-24 14:34:35 +0200

    added comment/ description

--- subqueries.h
+++ subqueries.h
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 
 
+/**
+ * Function getting a subquery in an embedded-query string. 
+ * E.g.: form "select * from TestPointList where id = (select ModId from DesignList where PicId = mid(PlanId, 3, 3))"
+ * the first argument "*szText" will be "select ModId from DesignList where PicId = mid(PlanId, 3, 3)"
+ * Supports as many levels as technically possible through using recurrence - for this, is szPos necessary.
+ * Call this function only with szPos = szCode: it will call itself with the currently calculated szPos
+ * See the comments inside the function(s) for details
+ */
 int getCurrentSubquery(char** szText, const char* szCode, const size_t codeSize, const char* szPos);
 
 
