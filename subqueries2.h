bb636cd230a9cc198364c7f97aaa39b01da3b18f
Claudiu Ciutacu <ciutacu@hotmail.com>
2019-06-24 14:35:56 +0200

    added comment/ description

--- subqueries2.h
+++ subqueries2.h
@@ -2,7 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+/**
+ * UNDER DEVELOPMENT! UNDER DEVELOPMENT! UNDER DEVELOPMENT! 
+ * See the function "getCurrentSubquery()"'s description. Intented to do the same.
+ */
 
 int getCurrentSubquery2(char** szText, const char* szCode, const size_t codeSize, const char* szPos);
 
