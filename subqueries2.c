5f4300e0033ea838c2ebffb29c3c12154887084d
Claudiu Ciutacu <ciutacu@hotmail.com>
2019-06-24 14:16:55 +0200

    asbuilt

---
+++ subqueries2.c
@@ -0,0 +1,11 @@
+
+
+#include "subqueries2.h"
+
+int getCurrentSubquery2(char** szText, const char* szCode, const size_t codeSize, const char* szPos)
+{
+    if(!szPos) return -3429;
+    
+    while(0);
+    
+}
