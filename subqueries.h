6dc83e75d9c540ddf7f9ad0233acaae77d04181c
Claudiu Ciutacu <ciutacu@hotmail.com>
2019-06-24 12:19:52 +0200

    Create subqueries.h

---
+++ subqueries.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int getCurrentSubquery(char** szText, const char* szCode, const size_t codeSize, const char* szPos);
+
+int getCurrentSubquery2(char** szText, const char* szCode, const size_t codeSize, const char* szPos);
